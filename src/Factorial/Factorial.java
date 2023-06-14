0690bdf9769ab8526627717dc59e69b660e78482
sunhitse <sunilakuthota@gmail.com>
2023-06-14 13:07:20 +0530

    factorial

---
+++ src/Factorial/Factorial.java
@@ -0,0 +1,20 @@
+package Factorial;
+
+class Factorial {
+    // method to find factorial of given number
+    static int factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+  
+        return n * factorial(n - 1);
+    }
+  
+    // Driver method
+    public static void main(String[] args)
+    {
+        int num = 6;
+        System.out.println("Factorial of " + num
+                           + " is " + factorial(6));
+    }
+}
